2727d6bc99f922ae69507090217bd225013ddf8e
ish10 <ishpreetsingh923@gmail.com>
2021-08-04 18:03:07 -0400

    register functionality

---
+++ CarRental.API/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using CarRental.API.Data;
+using CarRental.API.Dtos;
+using CarRental.API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public readonly CarRentalDbContext datacontext;
+
+        public AccountController(CarRentalDbContext datacontext)
+        {
+           
+            this.datacontext = datacontext;
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<UserDTO>> register(RegisterDTO registerDTO) {
+
+            using var hmac = new HMACSHA512();
+            var Appuser = new User
+            {
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.PasswordHash)),
+
+                FirstName = registerDTO.FirstName.ToLower(),
+                LastName = registerDTO.LastName.ToLower(),
+               Email = registerDTO.Email,
+                PhoneNumber = registerDTO.PhoneNumber
+
+            };
+            datacontext.Users.Add(Appuser);
+           await datacontext.SaveChangesAsync();
+            return new UserDTO { 
+            Username = registerDTO.FirstName + registerDTO.LastName,
+            Token = "JBCJBVJKBVJABVJLADVBJLBVLAJKBVLKABKLABVBLKAKLVAKVL"
+            };
+        }
+    }
+}
