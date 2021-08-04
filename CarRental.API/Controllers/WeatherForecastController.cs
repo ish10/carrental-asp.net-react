d7b2abb0f65e583632c9672509895d4b12faf860
raj raval <rajraval813@gmail.com>
2021-08-04 13:15:38 -0400

    Wetherforcast change DEMO

--- CarRental.API/Controllers/WeatherForecastController.cs
+++ CarRental.API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace CarRental.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "rain", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
