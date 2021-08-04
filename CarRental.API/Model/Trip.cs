23ca878896626bdc89c79ebb3d08a4bec167c904
raj raval <rajraval813@gmail.com>
2021-08-04 02:00:51 -0400

    Adding Model and DbContext class

---
+++ CarRental.API/Model/Trip.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.API.Model
+{
+    public class Trip
+    {
+        [Key]
+        public int TripId { get; set; }
+
+        [Required(ErrorMessage ="Enter valid start start")]
+        public DateTime StartDate { get; set; }
+        
+        [Required(ErrorMessage ="Enter valid End start")]
+        public DateTime EndDate { get; set; }
+
+      
+        public double TotalAmount { get; set; }
+
+        [Display(Name = "Car")]
+        public virtual int CarId { get; set; }
+
+        [ForeignKey("CarId")]
+        public virtual Car Car { get; set; }
+        
+        [Display(Name = "User")]
+        public virtual int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
+
+    }
+}
--- CarRental.API/Startup.cs
+++ CarRental.API/Startup.cs
@@ -1,7 +1,9 @@
+using CarRental.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,10 @@ namespace CarRental.API
         {
 
             services.AddControllers();
+            
+            services.AddDbContext<CarRentalDbContext>
+               (options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CarRental.API", Version = "v1" });
