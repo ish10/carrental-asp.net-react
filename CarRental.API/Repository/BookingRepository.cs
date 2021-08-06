e8eb994cb907077a9db556c2a9d5da97aeff3bee
MarwaMohamed <marwam.2@hotmail.com>
2021-08-05 21:13:41 -0500

    change return type from Task<ActionResult<string>> to list<carselectedobject> & filtered query according to city, location and isRented

--- CarRental.API/Repository/BookingRepository.cs
+++ CarRental.API/Repository/BookingRepository.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CarRental.API.Data;
 using CarRental.API.Model;
 using CarRental.API.Repository.IRepository;
+using CarRental.API.UtilitiesObjects;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace CarRental.API.Repository
 {
@@ -19,13 +23,36 @@ namespace CarRental.API.Repository
         }
 
 
-        public async Task<ActionResult<string>> GetData(ProvinceNames city, DateTime startDate, DateTime endDate, CarModel model)
+        public List<CarSelectedObject> GetData(FormData formdata)
         {
-            return trial(city, startDate, endDate, model);
+            var result = CarSet(formdata._cityName, formdata._startDate, formdata._endDate, formdata._Model);
+            return result;
         }
-        public string trial(ProvinceNames city, DateTime startDate, DateTime endDate, CarModel model)
+        private List<CarSelectedObject> CarSet(string city, DateTime startDate, DateTime endDate, CarModel model)
         {
-            return "city: " + city + " startDate: " + startDate + " endDate: " + endDate + " carModel:" + model;
+            List<CarSelectedObject> _result = new List<CarSelectedObject>();
+
+            var cars = (from carT in carRentalContext.Cars
+                       join loc in (from loc in carRentalContext.Locations
+                                    where loc.City.Equals(city)
+                                    select loc.LocationId)
+                       on carT.LocationId equals loc
+                       where carT.Model.Equals(model) && carT.IsRented.Equals(false)
+                        select new { carId = carT.CarId, model = carT.Model, pricaPerDay = carT.PricePerDay, plate = carT.NumberPlate, image = carT.Image }).ToArray();
+           
+            foreach (var c in cars)
+            {
+                _result.Add(new CarSelectedObject(c.carId, c.model, Enum.GetName(typeof(CarModel), c.model), c.pricaPerDay, c.image, c.image));
+            }
+
+
+            return _result;
         }
+
+        
     }
 }
+
+
+
+
