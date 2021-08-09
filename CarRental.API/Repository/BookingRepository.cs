b6450616bc577c144f87317c1c4504bb2476296b
MarwaMohamed <marwam.2@hotmail.com>
2021-08-08 22:49:13 -0500

    Got and check user Id within the post Json data + Insert the confirmed trip in the trip table + return the confirmed trip object

--- CarRental.API/Repository/BookingRepository.cs
+++ CarRental.API/Repository/BookingRepository.cs
@@ -28,79 +28,114 @@ namespace CarRental.API.Repository
             return result;
         }
 
-        public async Task ApplyBooking_and_confrimation(CarSelectedObject chosenCar)
+        public async Task<Trip> ApplyBooking_and_confrimation(CarSelectedObject chosenCar)
         {
             int TotalAmount = getTotalAmount(chosenCar);
 
+            //Insert the confirmed trip in the trip entity
+            var trip = await InsertIntoTripsDB(chosenCar, TotalAmount);
+
             //Get Single car which need to be updated and change the isRented field to true.
             await UpdateCarsDB(chosenCar);
-            
 
+            return trip;
         }
-
-        // Here we are comparing the sent JSON object with the entity, in order to check if any data is changed
-        public object[] CheckPostValid(CarSelectedObject chosenCar)
-        {
-            try
-            {
-                var car = (from carT in carRentalContext.Cars
-                           where carT.CarId.Equals(chosenCar.carId) && carT.Model.Equals(chosenCar.model)
-                                 && carT.PricePerDay.Equals(chosenCar.pricePerDay) && carT.Image.Equals(chosenCar.image)
-                                 && carT.NumberPlate.Equals(chosenCar.numberPlate)
-                           select carT).ToArray();
-                return car;
-            }
-            catch (Exception) {
-                return null;
-            }
-        }
-
-
 
 
 
         /*start of Class's dependent behaviours*/
-        //select list of cars from db
-         private List<CarSelectedObject> CarSet(string city, DateTime startDate, DateTime endDate, CarModel model)
-        {
-            List<CarSelectedObject> _result = new List<CarSelectedObject>();
-            try
-            {
-                var cars = (from carT in carRentalContext.Cars
-                            join loc in (from loc in carRentalContext.Locations
-                                         where loc.City.Equals(city)
-                                         select loc.LocationId)
-                            on carT.LocationId equals loc
-                            where carT.Model.Equals(model) && carT.IsRented.Equals(false)
-                            select new { carId = carT.CarId, model = carT.Model, pricaPerDay = carT.PricePerDay, plate = carT.NumberPlate, image = carT.Image }).ToArray();
 
-                foreach (var c in cars)
+            // Here we are comparing the sent JSON object with the entity, in order to check if any data is changed
+            public object[] CheckPostCarValid(CarSelectedObject chosenCar)
                 {
-                    _result.Add(new CarSelectedObject(c.carId, c.model, Enum.GetName(typeof(CarModel), c.model), c.pricaPerDay, c.image, c.plate, startDate, endDate));
+                    try
+                    {
+                        var car = (from carT in carRentalContext.Cars
+                                   where carT.CarId.Equals(chosenCar.carId) && carT.Model.Equals(chosenCar.model)
+                                         && carT.PricePerDay.Equals(chosenCar.pricePerDay) && carT.Image.Equals(chosenCar.image)
+                                         && carT.NumberPlate.Equals(chosenCar.numberPlate)
+                                   select carT).ToArray();
+                        return car;
+                    }
+                    catch (Exception)
+                    {
+                        return null;
+                    }
+                }
+
+            // Here we are checking that the user is in our db
+            public object CheckUserValid(CarSelectedObject userObject)
+            {
+                try
+                {
+                    var user = carRentalContext.Users.Where(u => u.UserId == userObject.userId).Select(u => u);
+                    return user;
+                }
+                catch (Exception)
+                {
+                    return null;
                 }
             }
-            catch (Exception error)
+
+            //select list of cars from db
+            private List<CarSelectedObject> CarSet(string city, DateTime startDate, DateTime endDate, CarModel model)
             {
-                _result = null;
+                List<CarSelectedObject> _result = new List<CarSelectedObject>();
+                try
+                {
+                    var cars = (from carT in carRentalContext.Cars
+                                join loc in (from loc in carRentalContext.Locations
+                                             where loc.City.Equals(city)
+                                             select loc.LocationId)
+                                on carT.LocationId equals loc
+                                where carT.Model.Equals(model) && carT.IsRented.Equals(false)
+                                select new { carId = carT.CarId, model = carT.Model, pricaPerDay = carT.PricePerDay, plate = carT.NumberPlate, image = carT.Image }).ToArray();
+
+                    foreach (var c in cars)
+                    {
+                        _result.Add(new CarSelectedObject(-1, c.carId, c.model, Enum.GetName(typeof(CarModel), c.model), c.pricaPerDay, c.image, c.plate, startDate, endDate));
+                    }
+                }
+                catch (Exception error)
+                {
+                    _result = null;
+                }
+                return _result;
             }
-            return _result;
-        }
 
-        //Update a car's isRented filed
-        private async Task UpdateCarsDB(CarSelectedObject chosenCar) {
-            var car = carRentalContext.Cars
-                        .Where(c => c.CarId == chosenCar.carId)
-                        .FirstOrDefault();
-            car.IsRented = true;
-            await carRentalContext.SaveChangesAsync();
-        }
+            //Update a car's isRented filed
+            private async Task UpdateCarsDB(CarSelectedObject chosenCar) {
+                var car = carRentalContext.Cars
+                            .Where(c => c.CarId == chosenCar.carId)
+                            .FirstOrDefault();
+                car.IsRented = true;
+                await carRentalContext.SaveChangesAsync();
+            }
 
-        //calculate TotalAmount = totalDays * priceperDay 
-        private int getTotalAmount(CarSelectedObject chosenCar)
-        {
-            int TotalDays = (int)(chosenCar.endDate.Date - chosenCar.startDate.Date).TotalDays;
-            return (int)chosenCar.pricePerDay * TotalDays; //e.g 30 dollars/day * 3 days = 90 dollars.
-        }
+            //Insert new trip in the trip entity
+            private async Task<Trip> InsertIntoTripsDB(CarSelectedObject chosenCar, int total_amount)
+            {
+                var trip = new Trip()
+                {
+                    StartDate = chosenCar.startDate,
+                    EndDate = chosenCar.endDate,
+                    TotalAmount = total_amount,
+                    CarId = chosenCar.carId,
+                    UserId = chosenCar.userId
+                };
+
+                await carRentalContext.Trips.AddAsync(trip);
+                await carRentalContext.SaveChangesAsync();
+                return trip;
+            }
+
+            //calculate TotalAmount = totalDays * priceperDay 
+            private int getTotalAmount(CarSelectedObject chosenCar)
+            {
+                int TotalDays = (int)(chosenCar.endDate.Date - chosenCar.startDate.Date).TotalDays;
+                return (int)chosenCar.pricePerDay * TotalDays; //e.g 30 dollars/day * 3 days = 90 dollars.
+            }
+
         /*end of Class's dependent behaviours*/
     }
 }
--- CarRental.API/UtilitiesObjects/CarSelectedObject.cs
+++ CarRental.API/UtilitiesObjects/CarSelectedObject.cs
@@ -8,6 +8,7 @@ namespace CarRental.API.UtilitiesObjects
 {
     public class CarSelectedObject
     {
+        public int userId { get; private set;} // when we send the car list will be -1 for booking, but will have a value when the user choose a car in confromTrip post 
         public int carId { get; private set; }
         public CarModel model { get; private set; }
         public string modelValue { get; private set; }
@@ -18,8 +19,10 @@ namespace CarRental.API.UtilitiesObjects
         public DateTime startDate { get; private set; }
         public DateTime endDate { get; private set; }
 
-        public CarSelectedObject(int carId, CarModel model, string modelValue, Double pricePerDay, string image, string numberPlate, DateTime startDate, DateTime endDate)
+
+        public CarSelectedObject(int userId, int carId, CarModel model, string modelValue, Double pricePerDay, string image, string numberPlate, DateTime startDate, DateTime endDate)
         {
+            this.userId = userId;
             this.carId = carId;
             this.model = model;
             this.modelValue = modelValue;
@@ -28,7 +31,7 @@ namespace CarRental.API.UtilitiesObjects
             this.numberPlate = numberPlate;
             this.startDate = startDate;
             this.endDate = endDate;
-                
+
         }
     }
 }
