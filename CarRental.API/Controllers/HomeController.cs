af027f9e286454100fd5a76efdd6ae04eaaf386c
raj raval <rajraval813@gmail.com>
2021-08-08 20:31:28 -0400

    Review&Home controller

---
+++ CarRental.API/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using CarRental.API.Model;
+using CarRental.API.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAllCars()
+        {
+
+            IEnumerable<Car> carsList = await _unitOfWork.Car.GetAllAsync(null, null, nameof(Location));
+            return new JsonResult(carsList);
+        }
+
+        [Route("FilterCarList")]
+        [HttpGet]
+        public async Task<IActionResult> GetFilterCars(ProvinceNames province ,string city,CarModel model)
+        {
+
+            // getting all car list 
+            IEnumerable<Car> carList = await _unitOfWork.Car.GetAllAsync(null, null, nameof(Location));
+
+            // return cars which mathces users selection of filter . 
+
+            IEnumerable<Car> FilterList = carList.Where(c => c.IsRented == false).Select(c=>c);
+            
+            if (model >= 0)
+            {
+                FilterList = FilterList.Where(c=>c.Model == model).Select(s=>s);
+
+
+                if (province >= 0)
+                {
+                    if (city != null)
+                    {
+                        FilterList = FilterList.Where(c => (c.Location.Province.Equals(province) && c.Location.City.Equals(city))).Select(c=>c);
+                    }
+                    else
+                    {
+                        FilterList = FilterList.Where(c => c.Location.Province.Equals(province)).Select(c => c);
+                    }
+                }
+                // Did't check for only city as dropdown for city is only available once they select any province.  
+            }
+            else
+            {
+                if (province >= 0)
+                {
+                    if (city != null)
+                    {
+                        FilterList = FilterList.Where(c => (c.Location.Province.Equals(province) && c.Location.City.Equals(city))).Select(c => c);
+                    }
+                    else
+                    {
+                        FilterList = FilterList.Where(c => c.Location.Province.Equals(province)).Select(c => c);
+                    }
+                }
+            }
+            
+            return new JsonResult(FilterList);
+        }
+
+
+
+    }
+}
---
+++ CarRental.API/Controllers/ReviewController.cs
@@ -0,0 +1,128 @@
+﻿using CarRental.API.Dtos;
+using CarRental.API.Model;
+using CarRental.API.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ReviewController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Review
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(Review))]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetReview()
+        {
+            IEnumerable<Review> ReviewList = await _unitOfWork.Review.GetAllAsync(null, null, nameof(Location));
+            return new JsonResult(ReviewList);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Car>> CreateReview([FromBody]ReviewDTO reviewDTO)
+        {
+            if(reviewDTO == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Review reviewobj = new Review()
+            {
+                starValue = reviewDTO.starValue,
+                Title = reviewDTO.Title,
+                Description = reviewDTO.Description,
+                PostTime = DateTime.Now
+            };
+
+            _unitOfWork.Review.AddReview(reviewobj);
+
+            
+            return Ok();
+        }
+
+        //PUT :api/Review/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReview(int id ,Review review)
+        {
+            if(id <= 0 || review.FeedbackId != id)
+            {
+                return BadRequest("Id is not valid or Feedback not found");
+            }
+
+
+            _unitOfWork.Review.Update(review);
+
+
+            try
+            {
+                await _unitOfWork.Review.Save();    //Seve the review 
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (await _unitOfWork.Review.DoesRecordExist(review.FeedbackId))
+                {
+                    return NotFound("review record does not exist or Not authorise to change");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
+        }
+
+
+        // DELETE: api/Review/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeleteCar(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("Id cannot be less than 0");
+            }
+
+            Review review = await _unitOfWork.Review.GetAsync(id);
+
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            await _unitOfWork.Review.RemoveAsync(review);
+            await _unitOfWork.Review.Save();
+
+            return NoContent();
+        }
+
+
+
+
+
+    }
+}
